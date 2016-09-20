345d06e12b4170436290abca3649cc301e2cf7a4
Andreas Schacherbauer <andreas.schacherbauer@wikitude.com>
2016-09-20 08:55:51 +0200

    Adds new required usage description .plist entries for iOS 10;
    Signed-off-by: Andreas Schacherbauer <andreas.schacherbauer@wikitude.com>

--- Scripts/WikitudePhoneGapSampleGenerator.sh
+++ Scripts/WikitudePhoneGapSampleGenerator.sh
@@ -129,8 +129,14 @@ if [ "true" == "$BUILD_IOS" ]; then
         sed -i.bak -e "s/ENTER-YOUR-KEY-HERE/${LICENSE_KEY//\//\/}/g" $INPUT_FILE && rm $INPUT_FILE.bak
     done
 
-    	# Add location access description key/value to info.plist
-	/usr/libexec/PlistBuddy -c "Add :NSLocationWhenInUseUsageDescription string 'Accessing GPS information is needed to display POIs around your current location'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+    # Add location access description key/value to info.plist
+	/usr/libexec/PlistBuddy -c "Add :NSLocationWhenInUseUsageDescription string 'Access to your current location information (GPS) is needed to find the nearest points of interest around your current position.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+
+	# Add camera access description key/value to info.plist
+	/usr/libexec/PlistBuddy -c "Add :NSCameraUsageDescription string 'Access to the camera is needed to display augmented reality content on top of your camera image.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+
+	# Add photo library access description key/value to info.plist
+	/usr/libexec/PlistBuddy -c "Add :NSPhotoLibraryUsageDescription string 'Access to your photo library is required for the example 'Bonus: Capture Screen' because it adds a screenshot of the current Architect view.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
 
     # Add enabled interface orientations to plist
     /usr/libexec/PlistBuddy -c "Add :UISupportedInterfaceOrientations array" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
